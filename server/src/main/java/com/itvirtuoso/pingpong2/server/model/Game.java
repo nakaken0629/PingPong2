672613d7b0117e2fd8d51858f213b65dbd0fd904
Nakagaki Kenji <nakagaki@a-tm.co.jp>
2015-02-09 14:02:17 +0900

    Playerを指定してGameインスタンスを生成する処理を作成

--- server/src/main/java/com/itvirtuoso/pingpong2/server/model/Game.java
+++ server/src/main/java/com/itvirtuoso/pingpong2/server/model/Game.java
@@ -4,14 +4,20 @@ package com.itvirtuoso.pingpong2.server.model;
  * Created by nakagaki on 2015/02/09.
  */
 public class Game {
-    private Game() {
-        /* nop */
+    private Player mPlayer1;
+
+    private Game(Player player1) {
+        mPlayer1 = player1;
     }
 
     public static Game create(Player player1) {
         if (player1 == null) {
             throw new IllegalArgumentException("player1 needs instance");
         }
-        return null;
+        return new Game(player1);
+    }
+
+    public Player getPlayer1() {
+        return mPlayer1;
     }
 }
--- server/src/test/java/com/itvirtuoso/pingpong2/server/model/GameTest.java
+++ server/src/test/java/com/itvirtuoso/pingpong2/server/model/GameTest.java
@@ -1,6 +1,7 @@
 package com.itvirtuoso.pingpong2.server.model;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by nakagaki on 2015/02/09.
@@ -9,5 +10,12 @@ public class GameTest {
     @Test(expected = IllegalArgumentException.class)
     public void 引数にnullを指定してGameオブジェクトを生成できない() throws Exception {
         Game game = Game.create(null);
+    }
+
+    @Test
+    public void 一人目のプレイヤーを指定してGameオブジェクトを生成する() throws Exception {
+        Player player = new Player();
+        Game game = Game.create(player);
+        assertEquals("指定した一人目のプレイヤーを取得できない", player, game.getPlayer1());
     }
 }
