15987019d4feb68206c4bd34fe7c47aaf601c7b3
Nakagaki Kenji <nakagaki@a-tm.co.jp>
2015-02-12 14:12:20 +0900

    emulatorとdevelopのflavorを作成

--- app/src/main/java/com/itvirtuoso/pingpong2/client/app/MainActivity.java
+++ app/src/main/java/com/itvirtuoso/pingpong2/client/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.itvirtuoso.pingpong2.client.app;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -14,6 +15,7 @@ import com.itvirtuoso.pingpong2.R;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +51,16 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void connect() {
+        String host = getString(R.string.server_host);
+        int port = Integer.parseInt(getString(R.string.server_port));
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        private MainActivity mActivity;
         private Button mConnectButton;
 
         public PlaceholderFragment() {
@@ -68,10 +76,16 @@ public class MainActivity extends ActionBarActivity {
             return rootView;
         }
 
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            mActivity = (MainActivity) activity;
+        }
+
         private class ConnectButtonClickListener implements View.OnClickListener {
             @Override
             public void onClick(View v) {
-
+                mActivity.connect();
             }
         }
     }
