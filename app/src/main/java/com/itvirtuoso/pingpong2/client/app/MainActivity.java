2e2d4df057926773ac307528abba00d62f08ff9d
Nakagaki Kenji <nakagaki@a-tm.co.jp>
2015-02-13 10:00:38 +0900

    不要なメニューを削除

--- app/src/main/java/com/itvirtuoso/pingpong2/client/app/MainActivity.java
+++ app/src/main/java/com/itvirtuoso/pingpong2/client/app/MainActivity.java
@@ -5,14 +5,11 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.itvirtuoso.pingpong2.R;
-
 
 public class MainActivity extends ActionBarActivity {
     private static final String TAG = MainActivity.class.getName();
@@ -26,34 +23,6 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void connect() {
-        String host = getString(R.string.server_host);
-        int port = Integer.parseInt(getString(R.string.server_port));
     }
 
     /**
@@ -85,7 +54,7 @@ public class MainActivity extends ActionBarActivity {
         private class ConnectButtonClickListener implements View.OnClickListener {
             @Override
             public void onClick(View v) {
-                mActivity.connect();
+                /* TOOD */
             }
         }
     }
